64083d25b1c5f1a6be1b33200a0ff344e0786495
Kilovata <89089449922@mail.ru>
2024-01-11 22:05:08 +0700

    Небольшие доработки в тексте.Начальная реализация механизма выхода из программы.

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        int exitPoint = 0;
-        while (exitPoint == 0) {
+        boolean exitPoint = true;
+        System.out.println("Для выхода введите 'q', для продолжения нажмите любую клавишу");
+        String stop;
+        stop = ScannerClass.scan.nextLine();
+        if(stop.equals("q")) {
+            exitPoint = false;
+        }
+        while (exitPoint) {
             double liter = 0;
             double distance = 0;
-
             System.out.print("Введите затраченное количество топлива: ");
             while (ScannerClass.scan.hasNext()) {
+                if(ScannerClass.scan.nextLine() == "q") {
+                    stop = ScannerClass.scan.nextLine();
+                    exitPoint = false;
+                    break;
+                }
                 if(ScannerClass.scan.hasNextDouble()) {
                     liter = ScannerClass.scan.nextDouble();
                     break;
                 } else {
-                    System.out.println("Ошибка! Необходимо ввести число.Повторите ввод: ");
+                    System.out.println("Ошибка! Необходимо ввести число. Повторите ввод: ");
                     ScannerClass.scan.next();
                 }
             }
             System.out.print("Введите пройденное растояние: ");
             while (ScannerClass.scan.hasNext()) {
+                if(ScannerClass.scan.nextLine() == "q") {
+                    stop = ScannerClass.scan.nextLine();
+                    exitPoint = false;
+                    break;
+                }
                 if(ScannerClass.scan.hasNextDouble()) {
                     distance = ScannerClass.scan.nextDouble();
                     break;
                 } else {
-                    System.out.println("Ошибка! Необходимо ввести число.Повторите ввод: ");
+                    System.out.println("Ошибка! Необходимо ввести число. Повторите ввод: ");
                     ScannerClass.scan.next();
                 }
             }
             double result = liter / distance * 100;
             result = Math.round(result * 100.0) / 100.0;
-            System.out.println("Ваш расход: " + result + "л/100км \n");
+            System.out.println("Ваш расход топлива: " + result + "л/100км \n");
         }
     }
 }
