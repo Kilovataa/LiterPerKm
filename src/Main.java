182bda18f9b3c100f0030897b9d5558769331e25
Kilovata <89089449922@mail.ru>
2024-01-11 22:39:13 +0700

    Добавлен функционал выхода из программы при вводе 'q'

--- src/Main.java
+++ src/Main.java
@@ -7,37 +7,36 @@ public class Main {
         if(stop.equals("q")) {
             exitPoint = false;
         }
+        outerLoop:
         while (exitPoint) {
             double liter = 0;
             double distance = 0;
             System.out.print("Введите затраченное количество топлива: ");
             while (ScannerClass.scan.hasNext()) {
-                if(ScannerClass.scan.nextLine() == "q") {
-                    stop = ScannerClass.scan.nextLine();
-                    exitPoint = false;
-                    break;
-                }
                 if(ScannerClass.scan.hasNextDouble()) {
                     liter = ScannerClass.scan.nextDouble();
                     break;
                 } else {
+                    if(ScannerClass.scan.next().equals("q")) {
+                        exitPoint = false;
+                        break outerLoop;
+                    }
                     System.out.println("Ошибка! Необходимо ввести число. Повторите ввод: ");
-                    ScannerClass.scan.next();
+                    //ScannerClass.scan.next();
                 }
             }
             System.out.print("Введите пройденное растояние: ");
             while (ScannerClass.scan.hasNext()) {
-                if(ScannerClass.scan.nextLine() == "q") {
-                    stop = ScannerClass.scan.nextLine();
-                    exitPoint = false;
-                    break;
-                }
                 if(ScannerClass.scan.hasNextDouble()) {
                     distance = ScannerClass.scan.nextDouble();
                     break;
                 } else {
+                    if(ScannerClass.scan.next().equals("q")) {
+                        exitPoint = false;
+                        break outerLoop;
+                    }
                     System.out.println("Ошибка! Необходимо ввести число. Повторите ввод: ");
-                    ScannerClass.scan.next();
+                    //ScannerClass.scan.next();
                 }
             }
             double result = liter / distance * 100;
